96bc0129fa5877018f731dd7db52851ab28f37d0
T-rex <0x50Fc@gmail.com>
2022-10-21 20:37:14 +0800

    Update Skill.tsx

--- src/page/skill/Skill.tsx
+++ src/page/skill/Skill.tsx
@@ -27,7 +27,7 @@ function SkillPage() {
     const navigate = useNavigate()
     const [state, setState] = useState<State>(State.None)
     const [errmsg, setErrmsg] = useState('')
-    const [task, setTask] = useState<Skill>()
+    const [skill, setSkill] = useState<Skill>()
     const [network,] = useNetwork()
     const [wallet,] = useWallet()
     const [isReady,] = useWalletReady()
@@ -57,7 +57,7 @@ function SkillPage() {
 
     if (state === State.None) {
         getSkill(id!).then((rs) => {
-            setTask(rs)
+            setSkill(rs)
             setState(State.Success)
         }, (reason) => {
             setErrmsg(getErrmsg(reason))
@@ -67,11 +67,11 @@ function SkillPage() {
     }
 
 
-    let newPullRequest = <></>
+    let grantButton = <></>
 
-    if (task && wallet) {
-        newPullRequest = <Link
-            to={"/task/pull/create?task_id=" + task!.id}
+    if (skill && wallet) {
+        grantButton = <Link
+            to={"/skill/grant?skill_id=" + skill!.id}
             className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
             {t('New pull request')}
@@ -103,7 +103,7 @@ function SkillPage() {
     } else {
 
         if (!balances) {
-            getBalances(task!.id, network!.currencys).then((rs) => {
+            getBalances(skill!.id, network!.currencys).then((rs) => {
                 setBlanances(rs)
             })
         }
@@ -111,19 +111,19 @@ function SkillPage() {
         card = <Card>
             <div className="flex flex-col items-center pb-10">
                 <div className="mb-3">
-                    <Logo addr={task!.erc721_name + '#' + task!.id} size="lg"></Logo>
+                    <Logo addr={skill!.erc721_name + '#' + skill!.id} size="lg"></Logo>
                 </div>
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    {task!.metadata ? task!.metadata.name : `${task!.gist_user}/${task!.gist_id}`}
+                    {skill!.metadata ? skill!.metadata.name : `${skill!.gist_user}/${skill!.gist_id}`}
                 </h5>
                 <span className="text-sm text-gray-500 dark:text-gray-400">
-                    {task!.erc721_name}#{task!.id}
+                    {skill!.erc721_name}#{skill!.id}
                 </span>
                 <ul className="divide-y divide-gray-200 dark:divide-gray-700 min-w-full">
                     <li className="py-3 sm:py-4 min-w-full cursor-pointer hover:opacity-75">
-                        <Link className="flex items-center space-x-4" to={'/task/pulls?task_id=' + task!.id}>
+                        <Link className="flex items-center space-x-4" to={'/skill/tasks?skill_id=' + skill!.id}>
                             <span className="shrink-0">
-                                <Logo addr={task!.erc721_name + '#' + task!.id} size="sm"></Logo>
+                                <Logo addr={skill!.erc721_name + '#' + skill!.id} size="sm"></Logo>
                             </span>
                             <span className="min-w-0 flex-1">
                                 <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
@@ -131,15 +131,15 @@ function SkillPage() {
                                 </p>
                             </span>
                             <span className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                <span>{task!.taskCount}</span>
+                                <span>{skill!.taskCount}</span>
                                 <HiOutlineChevronRight className='ml-2'></HiOutlineChevronRight>
                             </span>
                         </Link>
                     </li>
                     <li className="py-3 sm:py-4 min-w-full cursor-pointer hover:opacity-75">
-                        <Link className="flex items-center space-x-4" to={'/task/pulls?task_id=' + task!.id}>
+                        <Link className="flex items-center space-x-4" to={'/skill/grants?task_id=' + skill!.id}>
                             <span className="shrink-0">
-                                <Logo addr={task!.erc721_name + '#' + task!.id} size="sm"></Logo>
+                                <Logo addr={skill!.erc721_name + '#' + skill!.id} size="sm"></Logo>
                             </span>
                             <span className="min-w-0 flex-1">
                                 <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
@@ -147,7 +147,7 @@ function SkillPage() {
                                 </p>
                             </span>
                             <span className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                <span>{task!.taskCount}</span>
+                                <span>{skill!.taskCount}</span>
                                 <HiOutlineChevronRight className='ml-2'></HiOutlineChevronRight>
                             </span>
                         </Link>
@@ -155,7 +155,7 @@ function SkillPage() {
                     {
                         network!.currencys.map((item, index) => (
                             <li className="py-3 sm:py-4 min-w-full cursor-pointer hover:opacity-75" key={item.addr}>
-                                <Link className="flex items-center space-x-4" to={"/task/deposit?id=" + task!.id + '&currency=' + item.addr}>
+                                <Link className="flex items-center space-x-4" to={"/skill/deposit?id=" + skill!.id + '&currency=' + item.addr}>
                                     <span className="shrink-0">
                                         <Logo addr={item.addr} size="sm"></Logo>
                                     </span>
@@ -175,13 +175,13 @@ function SkillPage() {
                 </ul>
                 <div className="mt-4 flex space-x-3 lg:mt-6">
                     <Link
-                        to={"/skill/deposit?id=" + task!.id}
+                        to={"/skill/deposit?id=" + skill!.id}
                         className="inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                     >
                         {t('Deposit')}
                     </Link>
                     <a
-                        href={`https://gist.github.com/${task!.gist_user}/${task!.gist_id}`}
+                        href={`https://gist.github.com/${skill!.gist_user}/${skill!.gist_id}`}
                         target="_blank"
                         rel="noreferrer"
                         className="inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
@@ -194,7 +194,7 @@ function SkillPage() {
 
         gist = <Card>
             <ReactEmbedGist
-                gist={`${task!.gist_user}/${task!.gist_id}`}
+                gist={`${skill!.gist_user}/${skill!.gist_id}`}
                 wrapperClass="gist__bash"
                 loadingClass="mb-1 text-md font-medium text-gray-900 dark:text-white"
                 titleClass="gist__title"
@@ -211,7 +211,7 @@ function SkillPage() {
                 <div className='truncate font-medium text-3xl text-gray-900 dark:text-white flex-1 flex flex-row items-center'>
                     Skill
                 </div>
-                {newPullRequest}
+                {grantButton}
             </div>
             {failureAlert}
             <div className='pt-4'>
